0a7cfe42bb614c8eb11bb6bfafa336be364aa9bc
rajeev <rajeevhegde11@gmail.com>
2024-04-18 01:01:13 +0530

    pass 6th stage

--- app/server.go
+++ app/server.go
@@ -20,41 +20,43 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		go handleConn(conn)
 	}
-
-	handleConn(conn)
 }
 
 func handleConn(conn net.Conn) {
 	buf := make([]byte, 512)
-	n, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		return
-	}
-	req := string(buf[:n])
-	split_req := strings.Split(req, " ")
-	if split_req[1] == "/" {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	} else if strings.HasPrefix(split_req[1], "/echo") {
-		message := strings.Replace(split_req[1], "/echo/", "", 1)
-		res := response(message)
-		conn.Write([]byte(res))
-	} else if split_req[1] == "/user-agent" {
-		message := strings.Split(split_req[len(split_req)-2], "\r\n")
-		res := response(message[0])
-		conn.Write([]byte(res))
-	} else {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			return
+		}
+		req := string(buf[:n])
+		split_req := strings.Split(req, " ")
+		if split_req[1] == "/" {
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		} else if strings.HasPrefix(split_req[1], "/echo") {
+			message := strings.Replace(split_req[1], "/echo/", "", 1)
+			res := response(message)
+			conn.Write([]byte(res))
+		} else if split_req[1] == "/user-agent" {
+			message := strings.Split(split_req[len(split_req)-2], "\r\n")
+			res := response(message[0])
+			conn.Write([]byte(res))
+		} else {
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 
+		}
 	}
 }
-
 func response(message string) string {
 	statusLine := "HTTP/1.1 200 OK\r\n"
 	contentType := "Content-Type: text/plain\r\n"
