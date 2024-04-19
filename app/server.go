60ce771a60e50daddd8a42c260eff96e62d0be9a
rajeev <rajeevhegde11@gmail.com>
2024-04-19 19:53:52 +0530

    refactor

--- app/server.go
+++ app/server.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"strings"
 
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
@@ -12,8 +11,6 @@ import (
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
